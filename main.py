70e9a34437aa90a481d799ee5b7fc46ab3fe4ef3
Advik Singh Somvanshi <advik@criptext.com>
2021-08-23 19:32:58 +0530

    Code looks better now

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ import price as p
 bot = telebot.TeleBot('1806540764:AAHfQkvKp-CCzUyVCfTzTH7ePbQq2O8On9c', parse_mode='HTML')
 print("Bot started! Running...")
 
+wlcm_msg = """Welcome to the Superdoge Bot! Feel free to ask any questions you have in our <a href='https://t.me/superdogecoin'>Telegram Group</a>.
+
+<b>Website:</b> https://superdoge.com
+
+<b>Live on pancakeswap:</b> <a href='https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xC87A96F7af449aC18Ae59201dDE32e5A38A41C0F'>Buy Now!!</a>
+
+<b>About our project:</b> https://t.me/superdogecoin/20595
+
+<b>Blockchain Explorer:</b> https://explorer.superdoge.net/
+
+<b>Wallet Guides:-</b> /guides"""
+
+guide = """"<ins>The following guides are available related to SuperDoge Wallet:</ins>
+
+<b>Stuck Block:-</b> https://tiny.one/stuckblock
+<b>Masternode Link:-</b> https://tiny.one/masternode
+<b>Staking Link:-</b> https://tiny.one/staking
+<b>Wallet Guide:-</b> https://tiny.one/walletguide
+<b>Wallet Encryption Guide:-</b> https://tiny.one/encrypt
+<b>Backup & Restore Wallet:-</b> https://tiny.one/backupwallet
+
+In case you need any other guides or if you think any other topic would be useful, do write it in our group:)"""
+
+
 def newmsgs(message):
     for m in message:
         if message.chat.type == 'private':
@@ -17,21 +41,25 @@ def newmsgs(message):
 
 bot.set_update_listener(newmsgs)
 
+
 @bot.message_handler(content_types=['document'])
 def docmsg(message):
     m = message.chat.id
     bot.send_message(m, "I don't process documents but anyways, I'll keep it:) Thanks!")
 
+    
 @bot.message_handler(commands=['start', 'home', 'help'])
 def startmsg(message):
     m = message.chat.id
-    bot.send_message(m, "Heyo " + message.from_user.first_name + ", Welcome to the Superdoge Bot! Feel free to ask any questions you have in our <a href='https://t.me/superdogecoin'>Telegram Group</a>.\n\n<b>Website:</b> https://superdoge.com \n\n<b>Live on pancakeswap:</b> <a href='https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xC87A96F7af449aC18Ae59201dDE32e5A38A41C0F'>Buy Now!!</a> \n\n<b>About our project:</b> https://t.me/superdogecoin/20595\n\n<b>Blockchain Explorer:</b> https://explorer.superdoge.net/\n\n<b>Wallet Guides:-</b> /guides", disable_web_page_preview=True)
-
+    bot.send_message(m, "Heyo " + message.from_user.first_name + wlcm_msg, disable_web_page_preview=True)
+    
+    
 @bot.message_handler(commands=['guide', 'guides'])
 def guideslist(message):
     m = message.chat.id
-    bot.send_message(m, "<ins>The following guides are available related to SuperDoge Wallet:</ins> \n\n<b>Stuck Block:-</b> https://tiny.one/stuckblock \n<b>Masternode Link:-</b> https://tiny.one/masternode \n<b>Staking Link:-</b> https://tiny.one/staking \n<b>Wallet Guide:-</b> https://tiny.one/walletguide \n<b>Wallet Encryption Guide:-</b> https://tiny.one/encrypt \n<b>Backup & Restore Wallet:-</b> https://tiny.one/backupwallet \n\nIn case you need any other guides or if you think any other topic would be useful, do write it in our group:)", disable_web_page_preview=True)
-
+    bot.send_message(m, guide, disable_web_page_preview=True)
+    
+    
 @bot.message_handler(commands=['cmds', 'commands'])
 def cmds(message):
     m = message.chat.id
@@ -43,6 +71,7 @@ def cmds(message):
 🖼 /qr <code>address/text</code>: Generate QR Code for a specific address (BETA)
 💹 /price: Current price/details of SDOGE Token""")
 
+    
 @bot.message_handler(commands=['supply'])
 def supply(message):
     m = message.chat.id
@@ -50,6 +79,7 @@ def supply(message):
     circulating = circulating.text[:15]
     bot.send_message(m, "⚖️ Current circulation supply of SDOGE:" + circulating)
 
+    
 @bot.message_handler(commands=['balance'])
 def bal(message):
     m = message.chat.id
@@ -68,11 +98,13 @@ def bal(message):
     else:
         bot.send_message(m, "<b>Incorrect Address format!</b> \nMake sure the address starts with <b>S</b>.\nFor example: /balance ScAfTJwi37dFhsPEzkzn6zxtwqs1yHhoBP")
     
+    
 @bot.message_handler(commands=['p', 'price'])
 def price(message):
     m = message.chat.id
     bot.send_message(m, p.pricesdoge(), disable_web_page_preview=True)
 
+    
 @bot.message_handler(commands=['qr'])
 def qr(message):
     m = message.chat.id
@@ -86,6 +118,7 @@ def qr(message):
         qrimg.close()
     else:
         bot.send_message(m, "Dude, where's the text?")
+
 
 
 
